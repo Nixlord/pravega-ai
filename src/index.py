c347cbef52f3390ca63a38cf4c95a8e7bfba9c71
Shibasis Patnaik <shibasis0801@gmail.com>
2020-04-19 12:26:41 +0530

    comment

--- src/index.py
+++ src/index.py
@@ -3,6 +3,7 @@ import os
 import random
 from werkzeug.utils import secure_filename
 
+# This needs fixing. Random stuff happens while import. 
 from src.models.clock.getTime import detectTimeFrom
 
 app = Flask(__name__)
