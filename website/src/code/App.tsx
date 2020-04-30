11597e97b1e68d127831d94eda2393091de44318
Shibasis Patnaik <shibasis0801@gmail.com>
2020-04-30 19:00:52 +0530

    removed old code

--- website/src/code/App.tsx
+++ website/src/code/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 interface AppState {
@@ -25,7 +24,6 @@ export default class App extends React.Component<{}, AppState> {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hello {this.state.name}
           </p>
--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './code/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
