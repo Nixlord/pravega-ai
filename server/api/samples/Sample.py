52bfe2e347e09b62929ff7a8d543283883f47d05
Shibasis Patnaik <shibasis0801@gmail.com>
2020-05-19 01:06:30 +0530

    working blueprints

---
+++ server/api/samples/Sample.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request
+
+samples = Blueprint('samples', __name__)
+
+
+@samples.route('/hello/<name>')
+def say_hello(name: str):
+    return {"developer": name}
+
+
+# curl -X POST \
+#   http://localhost:5000/capitalize-post \
+#   -H 'Content-Type: application/json' \
+#   -H 'Host: localhost:5000' \
+#   -d '{"name": "shibasis"}'
+@samples.route('/capitalize-post', methods=['POST'])
+def sample_post():
+    data = request.get_json()
+    name: str = data['name'] or ""
+    return {'uppercaseName': name.upper()}
+
+
+@samples.route('/capitalize-get')
+def query():
+    name: str = request.args['name'] or ""
+    return {'uppercaseName': name.upper()}
--- server/index.py
+++ server/index.py
@@ -1,14 +1,13 @@
-from flask import Flask, jsonify, request, render_template
-import os
-import random
-from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template
 
-from .models.clock.getTime import detectTimeFrom
+from server.api.cv.ClockDetector import clock_detector
+from server.api.samples.Sample import samples
 
 print(__name__)
 print(__package__)
 
-app = Flask(__name__, template_folder='./build', static_folder='./build/static', static_url_path='/')
+app = Flask(__name__)
+# app = Flask(__name__, template_folder='./build', static_folder='./build/static', static_url_path='/')
 
 
 @app.route('/', defaults={'path': ''})
@@ -17,52 +16,14 @@ def render_react(path):
     return render_template("index.html")
 
 
-@app.route('/api/hello/<name>')
-def say_hello(name: str):
-    return jsonify({"developer": name})
-
-
-# curl -X POST \
-#   http://localhost:5000/capitalize-post \
-#   -H 'Content-Type: application/json' \
-#   -H 'Host: localhost:5000' \
-#   -d '{"name": "shibasis"}'
-@app.route('/api/capitalize-post', methods=['POST'])
-def sample_post():
-    data = request.get_json()
-    name: str = data['name'] or ""
-    return jsonify({'uppercaseName': name.upper()})
-
-
-@app.route('/api/capitalize-get')
-def query():
-    name: str = request.args['name'] or ""
-    return jsonify({'uppercaseName': name.upper()})
-
-
 # Learn at -> https://pythonise.com/series/learning-flask/flask-uploading-files
 @app.route('/upload-clock', methods=["GET"])
 def upload_clock_view():
     return render_template("uploadImage.html")
 
 
-@app.route('/api/upload-clock', methods=["POST"])
-def upload_clock():
-    if request.files:
-        image = request.files['clock_image']
-        # Of course a much better mechanism is required. This is PoC
-        filename = secure_filename(f'clock_image_{random.randint(1000, 9999)}.jpg')
-
-        if not os.path.exists('./temp'):
-            os.makedirs('./temp')
-
-        image_path = os.path.join('./temp', filename)
-        image.save(image_path)
-        print(f" Saving {image} as {image_path}")
-        time = detectTimeFrom(image_path)
-        print(f"Time: {time}")
-        return jsonify(time)
-
+app.register_blueprint(clock_detector, url_prefix='/api')
+app.register_blueprint(samples, url_prefix='/sample-api')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
