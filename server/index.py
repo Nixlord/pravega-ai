f6bcc990e627f9a2630f9a2fa7862e1cd323da0a
Shibasis <shibasis0801@gmail.com>
2020-07-15 20:23:40 +0530

    websockets demo

--- server/index.py
+++ server/index.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 import uvicorn
 
 app = FastAPI()
+
 
 #  check if websockets can do filehandling
 # i have remmoved apt buildpack will need to be restored before running open cv
@@ -11,6 +12,15 @@ async def homepage():
     return {
         "name": "shibasis"
     }
+
+
+@app.websocket("/ws/text")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
