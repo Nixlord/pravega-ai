d57e78dc6b0a16d7aee44c75411a23510edca014
Shibasis <shibasis0801@gmail.com>
2020-07-15 17:54:59 +0530

    aaaaargh

--- server/index.py
+++ server/index.py
@@ -3,7 +3,8 @@ import uvicorn
 
 app = FastAPI()
 
-
+#  check if websockets can do filehandling
+# i have remmoved apt buildpack will need to be restored before running open cv
 # API Register
 @app.get("/")
 async def homepage():
