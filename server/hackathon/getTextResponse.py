43bbab0839d4f6054a6924d052d4392d125ad854
Shibasis <shibasis0801@gmail.com>
2020-07-16 01:09:36 +0530

    kill me

--- server/hackathon/getTextResponse.py
+++ server/hackathon/getTextResponse.py
@@ -9,14 +9,14 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
 def send_text_dialogflow(text_to_be_analyzed):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-    try:
-        response = session_client.detect_intent(session=session, query_input=query_input)
-    except:
-        raise
+    # session_client = dialogflow.SessionsClient()
+    # session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    # text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    # query_input = dialogflow.types.QueryInput(text=text_input)
+    # try:
+    #     response = session_client.detect_intent(session=session, query_input=query_input)
+    # except:
+    #     raise
     # return {
     #     "text": response.query_result.query_text,
     #     "intent": response.query_result.intent.display_name,
