29a5f1e34a6fc87ba1e02dc85d47230050c31584
Shibasis <shibasis0801@gmail.com>
2020-07-16 00:51:12 +0530

    websockets demo

--- server/hackathon/__init__.py
+++ server/hackathon/__init__.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-if not os.environ["LOCAL"]:
+if os.environ.get("LOCAL") == "true":
     content = os.environ["GOOGLE_API_CREDENTIALS"]
     with open('credentials.json', 'w') as outfile:
         json.dump(content, outfile)
